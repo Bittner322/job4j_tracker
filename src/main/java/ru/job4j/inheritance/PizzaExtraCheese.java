575e42c9fb8eeed875d0216a859dec102ae268de
mikhail <sandreyf1972@gmail.com>
2024-11-21 22:14:29 +0600

    6. Вызов переопределенного метода родителя - super.method ... [#504871]

--- src/main/java/ru/job4j/inheritance/PizzaExtraCheese.java
+++ src/main/java/ru/job4j/inheritance/PizzaExtraCheese.java
@@ -1,9 +1,9 @@
 package ru.job4j.inheritance;
 
 public class PizzaExtraCheese extends Pizza {
-    private final String EXTRA_CHEESE_NAME = "extra cheese";
+    private final String extraCheeseName = "extra cheese";
 
     public String name() {
-        return super.name() + " " + EXTRA_CHEESE_NAME;
+        return super.name() + " " + extraCheeseName;
     }
 }
--- src/main/java/ru/job4j/inheritance/PizzaExtraCheeseExtraTomato.java
+++ src/main/java/ru/job4j/inheritance/PizzaExtraCheeseExtraTomato.java
@@ -1,9 +1,9 @@
 package ru.job4j.inheritance;
 
 public class PizzaExtraCheeseExtraTomato extends PizzaExtraCheese {
-    private final String EXTRA_CHEESE_EXTRA_TOMATO_NAME = "extra tomato";
+    private final String extraCheeseExtraTomatoName = "extra tomato";
 
     public String name() {
-        return super.name() + " " + EXTRA_CHEESE_EXTRA_TOMATO_NAME;
+        return super.name() + " " + extraCheeseExtraTomatoName;
     }
 }
