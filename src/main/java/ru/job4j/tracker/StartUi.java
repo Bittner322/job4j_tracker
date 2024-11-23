6258b8ab7784982b42ca41cb388f590a5ba77774
mikhail <sandreyf1972@gmail.com>
2024-11-23 14:52:49 +0600

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUi.java
+++ src/main/java/ru/job4j/tracker/StartUi.java
@@ -7,5 +7,7 @@ public class StartUi {
         Item item = new Item();
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         System.out.println(item.getCreated().format(dateTimeFormatter));
+
+        System.out.println(item);
     }
 }
