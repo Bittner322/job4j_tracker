46ca3dade6e34dc2b518d914061daea5c58e3f7a
mikhail <sandreyf1972@gmail.com>
2024-11-29 10:45:00 +0600

    0. Map, HashMap [#212652 #523912]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("asd@mail.com", "Иванов Иван Иванович");
+        map.put("fgh@mail.com", "Петров Петр Петрович");
+        map.put("jkl@mail.com", "Матвеев Матвей Матвеевич");
+        map.put("asd@mail.com", "Егоров Егор Егорович");
         for (String element : map.values()) {
             System.out.println(element);
         }
